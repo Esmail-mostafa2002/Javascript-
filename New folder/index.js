826ade1e9d0215dcf3075d52f81db641764de76c
esmail mostafa esmail <esmailalazaly2002@gmail.com>
2025-07-30 01:34:57 +0300

    Add files via upload

---
+++ New folder/index.js
@@ -0,0 +1,20 @@
+let x=10;
+let y=20;
+
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+add(3,5);
+subtract(10,4);
+
+
+
+
+let fruits = ["apple", "banana", "mango", "orange"];
+let result = fruits.slice(1, 3);
+
+console.log(result);  // ["banana", "mango"]
+console.log(fruits);  // الأصل لم يتغير
